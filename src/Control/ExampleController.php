bcefc63be5bfe1a861aaeb6eb0eda60388030490
Pascal Sancho <pascal.sancho@takoma.fr>
2022-01-14 15:01:01 +0100

    MAINT update lizyslim v9 (slim v4)

--- src/Control/ExampleController.php
+++ src/Control/ExampleController.php
@@ -2,9 +2,10 @@
 
 namespace Takoma\Template\Api\Control;
 
+use Psr\Http\Message\ResponseInterface;
+use RuntimeException;
 use Slim\App;
-use Slim\Http\Request;
-use Slim\Http\Response;
+use Slim\Http\ServerRequest;
 use Takoma\Lizy\SlimInterface\Control\BaseController;
 use Takoma\Lizy\Web\Json;
 use Takoma\Lizy\Web\StatusCode;
@@ -17,29 +18,31 @@ class ExampleController extends BaseController
      * GET /examples
      * @param array<string> $args
      */
-    public function get(Request $request, Response $response, array $args): Response
+    public function get(ServerRequest $request, ResponseInterface $response, array $args): ResponseInterface
     {
-        if (!$this->isJsonAccepted()) {
+        if (!self::acceptJson($request)) {
             return $response->withStatus(StatusCode::HTTP_406_NOT_ACCEPTABLE);
         }
 
         $json = new Json(['message' => Example::$message]);
 
-        return $response
-            ->withHeader('Content-Type', 'application/json')
-            ->write($json->serialize())
-        ;
+        $response->getBody()->write($json->serialize());
+        return $response->withHeader('Content-Type', 'application/json');
     }
 
     /** Définit les endpoints du contrôleur */
     public static function register(App $app): void
     {
         $container = $app->getContainer();
+        if (is_null($container)) {
+            throw new RuntimeException("app container is null");
+        }
         $securityHandler = $container->get('securityHandler');
 
-        $app->group('/examples', function () use ($app) {
+        $app->group('/examples', function ($app) {
             $app->get('', __CLASS__ . ':get');
         })
-        ->add($securityHandler);
+        ->add($securityHandler)
+        ;
     }
 }
--- src/Provider/SlimProvider.php
+++ src/Provider/SlimProvider.php
@@ -2,21 +2,21 @@
 
 namespace Takoma\Template\Api\Provider;
 
+use RuntimeException;
 use Slim\App;
-use Slim\Container;
+use Slim\Factory\AppFactory;
 use Takoma\Lizy\Conf;
 use Takoma\Lizy\Lizy;
 use Takoma\Lizy\SlimInterface\Control\VersionController;
 use Takoma\Lizy\SlimInterface\Middleware\CacheHandler;
 use Takoma\Lizy\SlimInterface\Middleware\CorsHandler;
 use Takoma\Lizy\SlimInterface\Middleware\ErrorHandler;
-use Takoma\Lizy\SlimInterface\Middleware\PhpErrorHandler;
 use Takoma\Lizy\SlimInterface\Middleware\SecurityHandler;
+use Takoma\Lizy\SlimInterface\Model\Container;
 use Takoma\Template\Api\Control\ExampleController;
 
 class SlimProvider
 {
-
     /** initialisation et lancement de l'app Slim */
     public static function init(): void
     {
@@ -27,24 +27,24 @@ class SlimProvider
         $overrideSecurity = (bool) Conf::get()->getOption('restApi', 'securityDisabled');
         $disableCache = Conf::get()->getOption('restApi', 'cacheDisabled', false);
 
-        $container = new Container([
-            'errorHandler' => function ($c) {
-                return new ErrorHandler;
-            },
-            'phpErrorHandler' => function ($c) {
-                return new PhpErrorHandler;
-            },
-            'securityHandler' => function ($c) use ($overrideSecurity) {
-                return new SecurityHandler(true, $overrideSecurity);
-            },
-                // toute entrée ici peut être récupérée par $container->get($key)
-        ]);
+        $container = new Container;
+        $container->set('securityHandler', new SecurityHandler(true, $overrideSecurity));
+        AppFactory::setContainer($container);
 
-        $app = new App($container);
+        $app = AppFactory::create();
+        $basepath = Conf::get()->getOption('slim', 'basepath');
+        if (is_null($basepath)) {
+            throw new RuntimeException("slim.basepath not set in config.ini.", 1);
+        }
+        $app->setBasePath($basepath);
 
+        $app->addRoutingMiddleware();
         // le dernier middleware ajouté est invoqué en 1er
         $app->add(new CorsHandler);
         $app->add(new CacheHandler($disableCache));
+
+        $errorMiddleware = $app->addErrorMiddleware(true, true, true);
+        $errorMiddleware->setDefaultErrorHandler(new ErrorHandler);
 
         static::registerControllers($app);
 
